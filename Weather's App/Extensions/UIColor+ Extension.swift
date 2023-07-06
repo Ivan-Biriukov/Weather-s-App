a5b5e1bd0e4226cbc1e318e6966a4d1145371ee9
Ivan Biriukov <Grottt0@mail.ru>
2023-07-06 17:48:53 +0300

    Pre build settings: - Add needed assets; - Add Fonts + Create Fonts Extension; - Create Colors Extension; - Create ViewController Extensions;

---
+++ Weather's App/Extensions/UIColor+ Extension.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+extension UIColor {
+    
+    // MARK: - ViewsBackground
+    
+    static let customViewsUnselectedBackground = UIColor(red: 50/256, green: 51/256, blue: 62/256, alpha: 1)
+    static let customViewsSelectedBackground = UIColor(red: 50/256, green: 51/256, blue: 62/256, alpha: 0.8)
+    static let verticalGrayLine = UIColor(red: 89/256, green: 92/256, blue: 102/256, alpha: 1)
+    static let verticalBrownLine = UIColor(red: 241/256, green: 178/256, blue: 137/256, alpha: 1)
+
+    
+    
+    
+    // MARK: - Text Colors
+    
+    static let darkText = UIColor(red: 10/256, green: 10/256, blue: 34/256, alpha: 1)
+    static let lightGrayText = UIColor(red: 139/256, green: 149/256, blue: 162/256, alpha: 1)
+    static let titleLightGray = UIColor(red: 115/256, green: 118/256, blue: 127/256, alpha: 1)
+    static let darkGrayText = UIColor(red: 155/256, green: 158/256, blue: 173/256, alpha: 1)
+    static let goldText = UIColor(red: 255/256, green: 189/256, blue: 0/256, alpha: 1)
+    static let linkedText = UIColor(red: 4/256, green: 221/256, blue: 242/256, alpha: 1)
+
+
+
+    
+    // NavBarTextColors
+    static let unselectedGray = UIColor(red: 134/256, green: 135/256, blue: 148/256, alpha: 1)
+
+}
+
---
+++ Weather's App/Extensions/UIFont+ Extension.swift
@@ -0,0 +1,76 @@
+import UIKit
+
+extension UIFont {
+    
+    // MARK: - Light
+    
+    static func poppinsLight10() -> UIFont?{
+        return UIFont(name: "Poppins-Light", size: 10)
+    }
+
+    // MARK: - Medium
+    
+    static func poppinsMedium10() -> UIFont?{
+        return UIFont(name: "Poppins-Medium", size: 10)
+    }
+    
+    static func poppinsMedium12() -> UIFont?{
+        return UIFont(name: "Poppins-Medium", size: 12)
+    }
+    
+    static func poppinsMedium14() -> UIFont?{
+        return UIFont(name: "Poppins-Medium", size: 14)
+    }
+    
+    // MARK: - Regular
+    
+    static func poppinsRegular8() -> UIFont?{
+        return UIFont(name: "Poppins-Regular", size: 8)
+    }
+    
+    static func poppinsRegular10() -> UIFont?{
+        return UIFont(name: "Poppins-Regular", size: 10)
+    }
+    
+    static func poppinsRegular12() -> UIFont?{
+        return UIFont(name: "Poppins-Regular", size: 14)
+    }
+    
+    static func poppinsRegular14() -> UIFont?{
+        return UIFont(name: "Poppins-Regular", size: 14)
+    }
+    
+    static func poppinsRegular16() -> UIFont?{
+        return UIFont(name: "Poppins-Regular", size: 16)
+    }
+    
+    static func poppinsRegular24() -> UIFont?{
+        return UIFont(name: "Poppins-Regular", size: 24)
+    }
+    
+    // MARK: - SemiBold
+    
+    static func poppinsSemiBold40() -> UIFont?{
+        return UIFont(name: "Poppins-SemiBold", size: 40)
+    }
+    
+    static func poppinsSemiBold28() -> UIFont?{
+        return UIFont(name: "Poppins-SemiBold", size: 28)
+    }
+    
+    static func poppinsSemiBold16() -> UIFont?{
+        return UIFont(name: "Poppins-SemiBold", size: 16)
+    }
+    
+    static func poppinsSemiBold14() -> UIFont?{
+        return UIFont(name: "Poppins-SemiBold", size: 14)
+    }
+    
+    static func poppinsSemiBold13() -> UIFont?{
+        return UIFont(name: "Poppins-SemiBold", size: 13)
+    }
+    
+    static func poppinsSemiBold12() -> UIFont?{
+        return UIFont(name: "Poppins-SemiBold", size: 12)
+    }
+}
