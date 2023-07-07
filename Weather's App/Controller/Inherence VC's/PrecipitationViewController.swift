f0e1e5aebc7c50f68f47e6ae455aac76e4875362
Ivan Biriukov <Grottt0@mail.ru>
2023-07-07 17:24:30 +0300

    Create a PageViewController + Create 3 VC to Present on it.

---
+++ Weather's App/Controller/Inherence VC's/PrecipitationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PrecipitationViewController.swift
+//  Weather's App
+//
+//  Created by иван Бирюков on 07.07.2023.
+//
+
+import UIKit
+
+class PrecipitationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Weather's App/Controller/MainViewController.swift
+++ Weather's App/Controller/MainViewController.swift
@@ -1,24 +1,66 @@
-
-
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UIPageViewControllerDelegate, UIPageViewControllerDataSource {
+    
+    var myController = [UIViewController]()
+    
+    // MARK: - UI Elements
 
+    
+    // MARK: - LifeCycle Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
-        // Do any additional setup after loading the view.
+        addVCsToPageController()
+        presentPageVC()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - PageController Section
+    
+    private func addVCsToPageController() {
+        myController.append(TodayViewController())
+        myController.append(ForecastViewController())
+        myController.append(PrecipitationViewController())
     }
-    */
+    
+    private func presentPageVC() {
+        guard let firstVC = myController.first else {return}
+        
+        let vc = UIPageViewController(transitionStyle: .pageCurl, navigationOrientation: .horizontal)
+        vc.delegate = self
+        vc.dataSource = self
+        vc.setViewControllers([firstVC], direction: .forward, animated: true)
+        vc.modalPresentationStyle = .fullScreen
+        present(vc, animated: true)
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        guard let index = myController.firstIndex(of: viewController), index > 0 else {
+            return nil
+        }
+        
+        let before = index - 1
+        return myController[before]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let index = myController.firstIndex(of: viewController), index < (myController.count - 1) else {
+            return nil
+        }
+        
+        let after = index + 1
+        return myController[after]
+    }
+    
+    // MARK: - Custom Buttons Methods
+    
+    
+
+    // MARK: - Configure UI
+    
+    
+  
 
 }
